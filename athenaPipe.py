0280d3c6e5331355840139d03d43da253f24d7c9
Michael Gogins <michael.gogins@gmail.com>
2021-04-11 21:27:44 -0400

    Remove unnecessary code.

--- athenaPipe.py
+++ athenaPipe.py
@@ -3,25 +3,6 @@ from tempfile import NamedTemporaryFile
 from athenaCL.libATH import athenaObj
 
 '''
-Executes a single athenaCL command and returns the result as a string.
-'''
-def process_command(command):
-    # Non-interactive interpretation.
-    interpreter = athenaObj.Interpreter('cgi')
-    return interpreter.cmd(command)
-
-'''
-Executes a list of one or more athenaCL commands and returns the results as a 
-string.
-'''
-def process_commands(commands):
-    interpreter = athenaObj.Interpreter()
-    result = []
-    for command in commands:
-        result.append(interpreter.cmd(command))
-    return result
-
-'''
 Processes a user-specified file of athenaCL commands, to print a 
 Csound score file.
 '''
@@ -51,6 +32,6 @@ def process(infile):
 args = sys.argv
 
 if len(args) != 2:
-	print("Error: Input Script Filename must be given")
+	print("Error: Input script filename must be given")
 else:
 	process(args[1])
